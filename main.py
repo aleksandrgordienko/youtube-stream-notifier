d017ae681ec180c18d4561e0373373588953a543
Aleksandr Gordienko <aleksandr.gordienko@yahoo.com>
2021-03-05 23:35:16 +0300

    Initial version

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+from time import sleep
+from telegram import Bot
+
+STREAM_LINK = os.environ.get('STREAM_LINK')
+TELEGRAM_CHANNEL = os.environ.get('TELEGRAM_CHANNEL')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHECK_PERIODICITY = 60  # seconds
+
+
+def channel_is_live(link):
+    try:
+        result = str(subprocess.check_output(['streamlink', link]))
+    except Exception as e:
+        print(e)
+
+    if "Available streams:" in result:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    state = False
+    while True:
+        if channel_is_live(STREAM_LINK):
+            if state is False:
+                tg_bot = Bot(token=TELEGRAM_TOKEN)
+                tg_bot.sendMessage(chat_id=TELEGRAM_CHANNEL, text=STREAM_LINK)
+                del tg_bot
+            state = True
+        else:
+            state = False
+        sleep(CHECK_PERIODICITY)
